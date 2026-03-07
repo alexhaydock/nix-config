5ca2b61c818b994b2166e10b38d17d31ed917706
Alex Haydock <alex@alexhaydock.co.uk>
2026-03-07 22:25:25 +0000

    Add Go & make

--- modules/pkgs-dev-workstation.nix
+++ modules/pkgs-dev-workstation.nix
@@ -23,7 +23,9 @@
       ghostscript # compress PDFs with: `gs -sDEVICE=pdfwrite -dPDFSETTINGS=/ebook -dNOPAUSE -dBATCH -sOutputFile=output.pdf input.pdf`
       gimp
       gitsign # Chainguard's git signing program: https://docs.sigstore.dev/cosign/signing/gitsign/
+      gnumake
       gnupg
+      go
       hadolint
       hyfetch
       iperf3
