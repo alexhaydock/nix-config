83126b92d4582e4a93ea07b474cc290ce8da04cd
Alex Haydock <alex@alexhaydock.co.uk>
2026-03-09 23:43:14 +0000

    Fix symlink

--- modules/symlink-downloads.nix
+++ modules/symlink-downloads.nix
@@ -3,7 +3,7 @@
   home-manager,
   ...
 }: {
-  home-mmanager.users.user = {
+  home-manager.users.user = {
     home.file = {
       "Downloads" = {
         source = config.lib.file.mkOutOfStoreSymlink "/home/user/zpools/data/x/Downloads";
