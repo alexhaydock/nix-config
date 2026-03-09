d3d03c45b89f4d5539793c86b9ae2b05ed1690dd
Alex Haydock <alex@alexhaydock.co.uk>
2026-03-09 16:56:30 +0000

    Add test VM config

---
+++ hosts/vm-test-x/default.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  pkgs,
+  nixpkgs,
+  nixpkgs-unstable,
+  home-manager,
+  ...
+}: {
+  networking.hostName = "x";
+
+  imports = [
+    ./hardware-configuration.nix
+    (import "${home-manager}/nixos")
+
+    ../../modules/cup-of-tea.nix
+    ../../modules/cups-disable.nix
+    ../../modules/generic.nix
+    ../../modules/gnome.nix
+    ../../modules/home-manager.nix
+    ../../modules/kernel-stable.nix
+    ../../modules/librewolf.nix
+    ../../modules/ntp-janet.nix
+    ../../modules/pipewire.nix
+    ../../modules/podman.nix
+    ../../modules/tor-browser.nix
+    ../../modules/unfree-allow.nix
+  ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+}
