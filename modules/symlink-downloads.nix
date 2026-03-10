da6a694c0eaf6e9a9a7c93445948a06beacc454d
Alex Haydock <alex@alexhaydock.co.uk>
2026-03-10 00:07:57 +0000

    Format

--- modules/symlink-downloads.nix
+++ modules/symlink-downloads.nix
@@ -1,9 +1,6 @@
 # I couldn't get mkOutOfStoreSymlink to work properly so here we slightly
 # abuse the systemd tmpfile generator to generate the symlink we need
-{
-  home-manager,
-  ...
-}: {
+{home-manager, ...}: {
   home-manager.users.user = {
     systemd.user.tmpfiles.rules = [
       "L /home/user/Downloads - - - - /home/user/zpools/data/x/Downloads"
