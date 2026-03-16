24523be387eab6c7049b55468c9ebd547996e8d6
Alex Haydock <alex@alexhaydock.co.uk>
2026-03-16 21:16:44 +0000

    Apply correct sourcetyping to Splunk forwarding config

--- modules/splunk-log-forwarding.nix
+++ modules/splunk-log-forwarding.nix
@@ -12,11 +12,19 @@
     # the following pages:
     # - https://syslog-ng.github.io/admin-guide/060_Sources/190_systemd-journal/README.html
     # - https://syslog-ng.github.io/admin-guide/070_Destinations/260_Splunk/000_Splunk-hec-event.html
+    #
+    # Note the use of '' before the $ variable in the sourcetype
+    # field is a Nix-ism and does not end up in the final
+    # syslog-ng config.
+    #
+    # Also note that the .journald.* prefix added to keys is the
+    # default used by syslog-ng and this is stripped by the rekey
+    # magic we've got going on in the event field.
     extraConfig = ''
       @version: 3.38
 
       source s_journald {
-        systemd-journal(prefix("journald."));
+        systemd-journal();
       };
 
       destination d_splunk_hec_event {
@@ -24,8 +32,8 @@
           url("https://splunk.infected.systems:8088")
           token("deadbeef-dead-beef-dead-beefdeadbeef")
           index("linux")
-          sourcetype("journald")
-          event("$(format_json --key journald.* --rekey journald.* --shift 9)\n")
+          sourcetype("''${.journald._TRANSPORT}")
+          event("$(format_json --key .journald.* --rekey .journald.* --shift 10)\n")
         );
       };
 
