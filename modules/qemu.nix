229b2ae02c71e0f4df236a1ac626f396ec7ab1ae
Alex Haydock <alex@alexhaydock.co.uk>
2026-03-29 12:14:12 +0100

    Update QEMU config

--- modules/qemu.nix
+++ modules/qemu.nix
@@ -9,8 +9,21 @@
 in {
   users.users.user.packages = with pkgs; [
     passt
-    qemu
-    unstable.OVMFFull
     unstable.quickemu
   ];
+
+  # Enable libvirtd
+  virtualisation.libvirtd = {
+    enable = true;
+    onShutdown = "shutdown"; # Shut down VMs when the host shuts down, rather than suspending
+    qemu.swtpm.enable = true;
+    qemu.package = pkgs.qemu_kvm; # Install QEMU only for native architecture for minimalism
+  };
+
+  # Allow unprivileged access to libvirtd
+  users.users.user = {
+    extraGroups = [
+      "libvirtd"
+    ];
+  };
 }
