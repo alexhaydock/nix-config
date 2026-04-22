70bac8c5f184036fe8a80108dade92fcc9b87a0b
Alex Haydock <alex@alexhaydock.co.uk>
2026-04-22 23:10:03 +0100

    Re-enable ROCm toggle for nixpkgs

--- modules/gpu-amd-rocm.nix
+++ modules/gpu-amd-rocm.nix
@@ -1,5 +1,10 @@
 {pkgs, ...}: {
+  # Enable ROCm support for system packages
+  # https://wiki.nixos.org/wiki/AMD_GPU#Enabling_ROCm_&_HIP_For_Packages
+  nixpkgs.config.rocmSupport = true;
+
+  # Install `rocminfo` for getting info about GPUs
   users.users.user.packages = with pkgs; [
-    rocmPackages.rocminfo # Install `rocminfo` for getting info about ROCm support
+    rocmPackages.rocminfo
   ];
 }
