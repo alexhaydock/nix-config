3c681b0a76b6ed4d779a8de8782b526284db5254
Alex Haydock <alex@alexhaydock.co.uk>
2026-05-18 09:14:55 +0100

    Use unstable Beets package

--- modules/beets.nix
+++ modules/beets.nix
@@ -1,7 +1,15 @@
-{home-manager, ...}: {
+{
+  pkgsUnstable,
+  home-manager,
+  ...
+}: {
   home-manager.users.user = {pkgs, ...}: {
     programs.beets = {
       enable = true;
+      package =
+        # Import newer Beets package due to Nix refusing to evaluate
+        # Beets 2.5.1 because of CVE-2026-42052
+        pkgsUnstable.beets;
       settings = {
         directory = "/home/user/zpools/data/media/music/navidrome/beets-managed";
         import = {
